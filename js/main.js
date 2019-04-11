2d155b48d1559a618756f474c6037aca51886e14
junior231 <43250479+junior231@users.noreply.github.com>
2019-04-11 16:19:00 -0400

    making subtle changes

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@
 	console.log("fired");
 
 
- 	const clickbio = document.querySelector('#click');
- 		  clickbio1 = document.querySelector('#click1');
-		  lightBox = document.querySelector('.lightbox');
-		  lightBox1 = document.querySelector('.lightbox1')
+ 	const clickbio = document.querySelector('#click'),
+ 		  clickbio1 = document.querySelector('#click1'),
+ 		  playVideo = document.querySelector('#play'),
+		  lightBox = document.querySelector('.lightbox'),
+		  lightBox1 = document.querySelector('.lightbox1'),
+		  lightbox3 = document.querySelector('.lightbox3'),
+		  picture = document.querySelector('.pictures')
+		  video = document.querySelector('video'),
+		  closeLB = document.querySelector('.lightbox-close');
 		  
 		  var button = document.querySelector("#button");
 		  var burgerCon = document.querySelector("#burgerCon");
@@ -15,8 +20,13 @@
 
 
 	function animImage() {
+		const offSet = 600;
+
 		TweenMax.to(logo, 0.7, {scaleX: 1.025, scaleY: 1.025, rotation: 360, ease:Elastic.easeInOut});
+
+		 picture.style.right = totalOffset;
 	}
+
 
 
 	function hamburgerMenu() {
@@ -43,6 +53,15 @@
  
 	}
 
+	function showLightbox3() {
+
+		hideLightbox ();
+
+ 
+		lightBox3.classList.add('display');
+ 
+	}
+
  
 	function hideLightbox() {
 
@@ -55,6 +74,9 @@
 	button.addEventListener("click", hamburgerMenu, false);
 	clickbio.addEventListener('click', showLightbox);
 	clickbio1.addEventListener('click', showLightbox1);
+	playVideo.addEventListener('click', showLightbox3);
+	video.addEventListener('ended', closeLightbox);
+	closeLB.addEventListener('click', closeLightbox);
 		
 })();
 
